34006e1946563626d44cc28de488d7e636b85ae7
Misha <hryvinskyim@gmail.com>
2025-01-06 17:08:40 +0200

    create data

---
+++ opt/src/JS/data.js
@@ -0,0 +1,58 @@
+export const arrRim = [
+    'Власна оправа 0', 
+    // A
+    'Alanie 595', 'AMSHAR 645', 'AMSHAR-lux  1445', 'AVL 525', 'Avtoman  250', 'Avtoman 295',
+    // C
+    'Caizi 195', 'CHARM 295', 'Choice 350', 'Calvin-Klein 2600',
+    // D
+    'Dacchi 400',  'Dior 2200', 'Dimarca 495', 'Dimarca 795',
+    // E
+    'Eclipse 645', 'Eclipse-lux 1100', 'Evision 1450', 'ENNI-MARCO 465',
+    // G
+    'Grand 200', 'Gladiator 550', 'Gladiator 1450', 
+    // J
+    'Jacopo 295', 
+    // K
+    'Kids-vision 350',
+    // M
+    'MIEN 325', 'MODNO 520',
+    // N
+    'Nikitana 635', 'Nikitana 645', 'Nikitana 1645',
+    // O
+    'OPTIMIX  465', 'OPTIMIX 695', 'OPTIMIX-lux  1300', 'Ovvio 925', 'Ovvio 795',
+    // P
+    'Panorama-Kids 445', 'Paolo-Rossi 380', 'Paolo-Rossi 520', 'Paolo-Rossi 950', 'Polaroid 465', 'Polaroid 950','PRADA 3500', 'Proud 900', 'RROVIZION 535', 'PROVIZION-(накладки) 2000', 'Pinguin-Baby 400', 'Pinguin-Baby 595', 'Pinguin-Baby 725',
+    // R
+    'Rich-Persons 645', 'Rich-Persons 1645', 'Ritmas 195', 'Red-Velvet 350', 'Red-Velvet 450', 'Red-Velvet 1100', 'Romeo 550',
+    // S
+    'Safllo 450', 'Safllo 950', 'SunRays 250', 'Santarelly 3000', 'Santarelly 2000',  'Senso 695', 'Senso 725', 'Senso 925', 'Senso 1500', 'Sone 305',
+    // T
+    'Tommy-Fashion 645', 'Tonjia 195', 
+    // V
+    'Victory 196', 
+    // Z
+    'Zenzero 465',
+];
+
+export const arrJob = [
+    {
+        id: "job1",
+        price: "200",
+        text: "Робота техніка",
+    },
+    {
+        id: "job2",
+        price: "300",
+        text: "Власна оправа",
+    },
+    {
+        id: "job3",
+        price: "250",
+        text: "Лєска"
+    },
+    {
+        id: "job4",
+        price: "300",
+        text: "Біфокали"
+    }
+];
