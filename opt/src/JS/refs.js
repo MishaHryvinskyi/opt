b55a8fb5703911e6ddd62320715f29f2ffcec77a
Misha <hryvinskyim@gmail.com>
2025-01-07 17:06:29 +0200

    added refs

---
+++ opt/src/JS/refs.js
@@ -0,0 +1,20 @@
+export const refs = {
+    // form
+    form: document.querySelector('.js-form'),
+    userName: document.querySelector('.user-name'),
+    userNumber: document.querySelector('.user-number'),
+    comment: document.querySelector('.comment'),
+    rimPrice: document.querySelector('.user-rim-price'),
+    lenses: document.querySelector('.user-lenses'),
+    lensesOD: document.querySelector('.user-lenses-od'),
+    lensesOS: document.querySelector('.user-lenses-os'),
+    ton: document.querySelector('.ton'),
+    urgency: document.querySelector('.urgency'),
+    radioEl: document.querySelector('.radio-js'),
+    closeModalBtn: document.querySelector('.close-btn'),
+    // table
+    tBody: document.querySelector('.t-body'),
+    // modal
+    modalBackdrop: document.querySelector('.backdrop'),
+    userOrder: document.querySelector('.user-order'),
+}
