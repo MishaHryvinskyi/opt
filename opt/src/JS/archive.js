7e45a34155bbaebc6fad38a7e9b7a7d5997b0982
Misha <hryvinskyim@gmail.com>
2025-01-08 18:43:15 +0200

    added func remove tr

--- opt/src/JS/archive.js
+++ opt/src/JS/archive.js
@@ -1,8 +1,10 @@
 import { getUsers } from "../API/api";
-import { refs } from "./refs";
 import { createMarkup } from "./markup";
+import { refs } from "./refs";
+import { onRemoveClick } from "./func";
 
 getUsers().then(data => {
     const markup = data.map(createMarkup).join('');
     refs.tBody.insertAdjacentHTML('beforeend', markup);
+    refs.tBody.addEventListener('click', onRemoveClick);
 });
--- opt/src/JS/func.js
+++ opt/src/JS/func.js
@@ -1,3 +1,7 @@
+import Notiflix from 'notiflix';
+
+import { removeUser } from '../API/api';
+
 export const myltiply = (a, b, c, d, e) => Number(a) + Number(b) + Number(c) + Number(d) + Number(e);
 
 export const getSelectedRadioValue = (name) => {
@@ -10,7 +14,21 @@ export const currentDate = () => {
     const currentMounth = date.getMonth();
     const currentDate =  date.getDate();
     const currentYear = date.getFullYear();
-    const currentHours = date.getHours();
-    const currentMinuts = date.getMinutes();
-    return `${currentDate.toString().padStart(2, '0')}.${(currentMounth + 1).toString().padStart(2, '0')}.${currentYear} ${currentHours.toString().padStart(2, '0')}:${currentMinuts.toString().padStart(2, '0')}`;
+    return `${currentDate.toString().padStart(2, '0')}.${(currentMounth + 1).toString().padStart(2, '0')}.${currentYear}`;
 };
+
+export function onRemoveClick ({ target }) {
+    if(target.closest('.remove-btn')) {
+        const tr = target.closest('tr');
+        const userId = tr.id;
+
+        removeUser(userId)
+            .then(() => {
+                tr.remove();
+                Notiflix.Notify.success(`Замовлення № ${userId} успішно видалений!`);
+            })
+            .catch(error => {
+                console.error('Помилка видалення користувача:', error);
+            });
+    }
+}
